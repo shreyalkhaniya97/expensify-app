39583eec133b8b8c34cbcaeffbae8fd308ec2eb9
alkhaniya <aditya.alkhaniya@gmail.com>
2020-09-14 15:35:06 +0530

    setup productino build and server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const publicPath = path.join(__dirname, "..", "public");
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  return res.sendFile(path.join(publicPath, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("server is running at ", PORT);
+});
